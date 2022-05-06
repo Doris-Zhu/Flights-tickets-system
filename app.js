e01cd4eb477e3644dcf87fd452c47a9683ec10ed
bian-hengwei <55875341+bian-hengwei@users.noreply.github.com>
2022-05-06 12:03:31 +0800

    feat. seperate login and register for different roles

--- app.js
+++ app.js
@@ -19,18 +19,57 @@ app.use(session({
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/login', (req, res) => {
-    res.render('role', { message: 'test message', op: 'login' });
+app.get('/', (req, res) => {
+    res.render('home', {  });
 });
 
-app.post('/login', (req, res) => {});
+app.get('/login', (req, res) => {
+    if (req.query.role === undefined) {
+        res.render('role', { op: 'login' });
+    }
+    else {
+        if (req.query.role === 'Customer') {
+            res.render('customer_login', {  });
+        }
+        else if (req.query.role === 'Agent') {
+            res.render('agent_login', {});
+        }
+        else if (req.query.role === 'Staff') {
+            res.render('staff_login', {});
+        }
+        else {
+            throw new Error();
+        }
+    }
+});
+
+// handle login
+app.post('/login', (req, res) => {
+    res.render('login', {  });
+});
 
 app.get('/register', (req, res) => {
-    res.render('role', {op: 'login'});
-})
+    if (req.query.role === undefined) {
+        res.render('role', { op: 'register' });
+    }
+    else {
+        if (req.query.role === 'Customer') {
+            res.render('customer_register', {  });
+        }
+        else if (req.query.role === 'Agent') {
+            res.render('agent_register', {});
+        }
+        else if (req.query.role === 'Staff') {
+            res.render('staff_register', {});
+        }
+        else {
+            throw new Error();
+        }
+    }
+});
 
 const handleRegister = async function(req, res) {
-    const {username, password, role} = req.body;
+    const { username, password, role } = req.body;
     if (username === '') {
         res.render('register', { message: 'Username is required' });
         return;
